f74fc42f10f81b11f17d247322616adab761cc0b
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-07-25 00:50:56 +0430

    made validate data function to check and interact with modal data

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import { openModal, closeModal } from "./modal.js";
+import { openModal, closeModal, validateData } from "./modal.js";
 
 const footerBtn = document.querySelector(".footer__btn");
 footerBtn.addEventListener("click", openModal);
@@ -7,3 +7,11 @@ window.addEventListener("click", (e) => {
   const modal = document.querySelector(".modal");
   if (e.target == modal) closeModal();
 });
+
+const modalAddBtn = document.querySelector(".modal__button");
+modalAddBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  validateData();
+});
+// add enter event listener
--- src/js/modal.js
+++ src/js/modal.js
@@ -7,3 +7,38 @@ export const closeModal = () => {
   const modal = document.querySelector(".modal");
   modal.style.display = "none";
 };
+
+export const validateData = () => {
+  const todoInput = document.querySelector(".modal__input");
+  const dateInput = document.querySelector(".modal__date");
+
+  todoInput.addEventListener("keydown", () => {
+    removeErrorClass();
+  });
+  dateInput.addEventListener("click", (e) => {
+    removeErrorClass();
+  });
+
+  if (!todoInput.value) {
+    todoInput.classList.add("error");
+    return;
+  }
+
+  if (!dateInput.value) {
+    dateInput.classList.add("error");
+    return;
+  }
+
+  makeTask();
+};
+
+const makeTask = () => {
+  removeErrorClass();
+
+  console.log("also here");
+};
+
+const removeErrorClass = () => {
+  document.querySelector(".modal__input").classList.remove("error");
+  document.querySelector(".modal__date").classList.remove("error");
+};
