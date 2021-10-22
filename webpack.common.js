e32b9d81e36091c7ab4223b9b32abfde69db3a30
Mahdi Aghaei <mahd.aghaei@gmail.com>
2021-10-22 13:24:27 +0330

    made dev and prod config

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,8 @@
-const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
     main: "./src/index.js",
-  },
-  output: {
-    filename: "main-[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
